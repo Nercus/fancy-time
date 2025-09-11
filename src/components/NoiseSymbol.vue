3787da9b70c7ad0c7612ccf5750338441be98a85
Nercus <48932939+Nercus@users.noreply.github.com>
2025-09-11 17:16:52 +0200

    fix: set will-transform on motion-v element

--- src/components/NoiseSymbol.vue
+++ src/components/NoiseSymbol.vue
@@ -1,7 +1,7 @@
 <!-- eslint-disable vue/component-name-in-template-casing -->
 <template>
   <motion.div
-    class="flex flex-col content-start" :animate="{ y }" :transition="{ type: 'spring', duration: 0.75, bounce: 0.5 }">
+    class="flex flex-col content-start will-change-transform" :animate="{ y }" :transition="{ type: 'spring', duration: 0.75, bounce: 0.5 }">
     <span v-for="el in range" :key="el" :class="[el === props.symbol ? 'opacity-100' : 'opacity-30']" class="flex justify-center items-center rounded font-semibold text-rose-800 text-5xl tracking-widest transition-opacity duration-250 select-none">
       {{ Number(el) ? Number(el) : el }}
     </span>
