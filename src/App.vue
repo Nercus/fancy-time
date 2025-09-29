3cbe8520b87d658329762f1fff0cff96dbd1a622
Nercus <48932939+Nercus@users.noreply.github.com>
2025-09-29 11:28:23 +0200

    chore: remove slide animation and use fade

--- src/App.vue
+++ src/App.vue
@@ -1,107 +1,12 @@
 <template>
   <RouterView v-slot="{ Component }">
-    <Transition :name="transitionName" :mode="transitionMode">
+    <Transition name="fade" mode="out-in">
       <component :is="Component" />
     </Transition>
   </RouterView>
 </template>
 
-<script setup lang="ts">
-import { breakpointsTailwind } from '@vueuse/core'
-import { ref, watch } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-
-const { isSmallerOrEqual } = useBreakpoints(breakpointsTailwind)
-const route = useRoute()
-const router = useRouter()
-const transitionName = ref('slide-forward')
-const transitionMode = ref<'default' | 'out-in'>('default')
-
-let previousOrder = route.meta.order || 0
-
-watch(route, (newRoute) => {
-  const routes = router.getRoutes()
-  const maxOrder = Math.max(...routes.map(r => (r.meta.order as number) ?? 0))
-  const currentOrder = newRoute.meta.order || 0
-
-  if (isSmallerOrEqual('sm')) {
-    transitionName.value = 'fade'
-    transitionMode.value = 'out-in'
-    previousOrder = currentOrder
-    return
-  }
-
-  transitionMode.value = 'default'
-  if (currentOrder === 0 && previousOrder === maxOrder) {
-    // Wrap-around: last to first (forward)
-    transitionName.value = 'slide-forward'
-  }
-  else if (currentOrder === maxOrder && previousOrder === 0) {
-    // Wrap-around: first to last (backward)
-    transitionName.value = 'slide-backward'
-  }
-  else if (currentOrder > previousOrder) {
-    transitionName.value = 'slide-forward'
-  }
-  else {
-    transitionName.value = 'slide-backward'
-  }
-
-  previousOrder = currentOrder
-})
-</script>
-
 <style scoped>
-.slide-forward-enter-active,
-.slide-forward-leave-active {
-  transition: all 0.75s ease-out;
-}
-
-.slide-forward-enter-from {
-  position: absolute;
-  right: -100%;
-}
-
-.slide-forward-enter-to {
-  position: absolute;
-  right: 0;
-}
-
-.slide-forward-leave-from {
-  position: absolute;
-  left: 0;
-}
-
-.slide-forward-leave-to {
-  position: absolute;
-  left: -100%;
-}
-
-.slide-backward-enter-active,
-.slide-backward-leave-active {
-  transition: all 0.75s ease-out;
-}
-
-.slide-backward-enter-from {
-  position: absolute;
-  left: -100%;
-}
-
-.slide-backward-enter-to {
-  position: absolute;
-  left: 0;
-}
-
-.slide-backward-leave-from {
-  position: absolute;
-  right: 0;
-}
-
-.slide-backward-leave-to {
-  position: absolute;
-  right: -100%;
-}
-
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.5s;
